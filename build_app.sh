9637bc1e610962ab3a8714a7374d1d18d01696d2
bni <bnilsson@me.com>
2026-01-07 15:25:52 +0100

    Copy needed files to build directory

--- build_app.sh
+++ build_app.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 rm -f Flashback.app/Contents/MacOS/rs
-cp cmake-build-release/rs Flashback.app/Contents/MacOS/
-cp rs.cfg Flashback.app/Contents/Resources/
-cp vertex.shader Flashback.app/Contents/Resources/
-cp pixel.shader Flashback.app/Contents/Resources/
-cp -r DATA Flashback.app/Contents/Resources/
+cp build/rs Flashback.app/Contents/MacOS/
+cp build/rs.cfg Flashback.app/Contents/Resources/
+cp build/vertex.shader Flashback.app/Contents/Resources/
+cp build/pixel.shader Flashback.app/Contents/Resources/
+cp -r build/DATA Flashback.app/Contents/Resources/
 ../macdylibbundler/dylibbundler -od -b -x Flashback.app/Contents/MacOS/rs -d ./Flashback.app/Contents/libs/
 otool -L Flashback.app/Contents/MacOS/rs
